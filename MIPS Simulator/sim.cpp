6a9e72404f0f358648a4e7600d9b2036ecdc9641
Andrew Wood <AJWOOD@andrew-woods-imac.local>
2017-09-24 12:25:46 -0400

    Initial Commit.

--- MIPS Simulator/sim.cpp
+++ MIPS Simulator/sim.cpp
@@ -1,15 +1,15 @@
 //
-//  main.cpp
+//  sim.cpp
 //  MIPS Simulator
 //
 //  Created by Andrew Wood on 9/24/17.
 //  Copyright © 2017 FSU. All rights reserved.
 //
 
+#include <stdio.h>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    std::cout << "Hello World!\n";
 }
